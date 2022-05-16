178d85fe20e9ebc7bfb49d605a95c36b8e119523
Reisliebhaber <pmntt21@gmail.com>
2022-05-16 14:14:27 +0200

    gradle Project initialization with holiday process bpmn-model

---
+++ src/test/java/com/workflow/holidayRequest/HolidayRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.holidayRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
